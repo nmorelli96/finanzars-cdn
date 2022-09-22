c1dea0a66dce25f3dddd7b3efb4e6c16bfc3c70d
nmorelli96 <morellinicolas96@gmail.com>
2022-09-21 23:05:31 -0300

    Change binance API function structure, Add cryptos APIs

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ class Fiat extends React.Component {
 								style={{ color: new Date().getTime() - (fiat.time * 1000) > 3600000 ? 'red' : 'green' }}>
 								{new Date(fiat.time * 1000).toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' })}
 							</th>
-							<th>Venta</th>
+							<th><button style={{ cursor: 'default' }}>Venta</button></th>
 						</tr>
 					</thead>
 					<tbody>
@@ -114,6 +114,17 @@ class Bancos extends React.Component {
 
 class Crypto extends React.Component {
 	render() {
+
+		const {
+			binanceUSDTBuy,
+			binanceUSDTSell,
+			binanceDAIBuy,
+			binanceDAISell,
+			cryptos
+		} = this.props;
+
+		let sortedCryptos = [...cryptos]
+
 		return (
 			<div>
 				<div id="cryptoContainer">
@@ -127,23 +138,55 @@ class Crypto extends React.Component {
 									</button>
 								</th>
 								<th>
-									<button type="button" onClick={() => this.props.sortByCompra()}>
-										USDT/ARS <i class="fa-solid fa-sort fa-xs"></i>
+									<button type="button" onClick={() => this.props.sortByVenta()}>
+										Coin <i class="fa-solid fa-sort fa-xs"></i>
 									</button>
 								</th>
-								{/*<th>
-									<button type="button" onClick={() => this.props.sortByVenta()}>
+								<th>
+									<button type="button" onClick={() => this.props.sortByCompra()}>
+										Compra <i class="fa-solid fa-sort fa-xs"></i>
+									</button>
+								</th>
+								<th>
+									<button type="button" onClick={() => this.props.sortByCompra()}>
 										Venta <i class="fa-solid fa-sort fa-xs"></i>
 									</button>
-		
-								<th><button style={{ cursor: 'default' }}>Hora</button></th></th>*/}
+								</th>
+								<th>
+									<button type="button" onClick={() => this.props.sortByCompra()}>
+										Hora <i class="fa-solid fa-sort fa-xs"></i>
+									</button>
+								</th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
 								<td>Binance P2P</td>
-								<td>{this.props.binanceP2P.precio}</td>
+								<td>{binanceUSDTBuy.coin}</td>
+								<td>{parseFloat(binanceUSDTSell.precio).toFixed(2)}</td>
+								<td>{parseFloat(binanceUSDTBuy.precio).toFixed(2)}</td>
+								<td>-</td>
 							</tr>
+							<tr>
+								<td>Binance P2P</td>
+								<td>{binanceDAIBuy.coin}</td>
+								<td>{parseFloat(binanceDAISell.precio).toFixed(2)}</td>
+								<td>{parseFloat(binanceDAIBuy.precio).toFixed(2)}</td>
+								<td>-</td>
+							</tr>
+							{sortedCryptos.map(exchange => (
+							<tr key={exchange.id}>
+								<td>{exchange.banco}</td>
+								<td>{exchange.coin}</td>
+								<td>{exchange.compra.toFixed(2)}</td>
+								<td>{exchange.venta.toFixed(2)}</td>
+								<td
+									title={new Date(exchange.time * 1000).toLocaleString('es-AR')}
+									style={{ color: new Date().getTime() - (exchange.time * 1000) > 3600000 ? 'red' : 'green' }}>
+									{new Date(exchange.time * 1000).toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' })}{/*, {new Date(banco.time * 1000).toLocaleDateString('es-AR')}*/}
+								</td>
+							</tr>
+						))}
 						</tbody>
 					</table>
 				</div>
@@ -159,22 +202,65 @@ class App extends React.Component {
 		this.state = {
 			fiat: { "oficial": 0, "solidario": 0, "blue": 0, "ccb": 0, "mep": 0, "ccl": 0, "mepgd30": 0, "cclgd30": 0, "time": 0 },
 			bancos: [],
-			binanceP2P: [],
-			sortConfig: { key: null, direction: 'ascending' }
+			binanceUSDTBuy: [],
+			binanceUSDTSell: [],
+			binanceDAIBuy: [],
+			binanceDAISell: [],
+			cryptos: [],
+			sortConfig: { key: 'ventaTot', direction: 'ascending' }
 		};
-		this.componentDidMount = this.componentDidMount.bind(this);
-		this.getFiatFromAPI = this.getFiatFromAPI.bind(this);
-		this.getBancosFromAPI = this.getBancosFromAPI.bind(this);
 		this.sortByEntidad = this.sortByEntidad.bind(this);
 		this.sortByCompra = this.sortByCompra.bind(this);
 		this.sortByVenta = this.sortByVenta.bind(this);
 	}
 
 	componentDidMount() {
-		document.addEventListener("DOMContentLoaded", this.getFiatFromAPI());
-		document.addEventListener("DOMContentLoaded", this.getBancosFromAPI());
-		document.addEventListener("DOMContentLoaded", this.getBinanceP2PFromAPI());
+		document.addEventListener("DOMContentLoaded", this.loadAPIs());
 	}
+
+	loadAPIs() {
+		this.getFiatFromAPI();
+		this.getBancosFromAPI();
+		this.getBinanceP2PFromAPI('buy', 'usdt');
+		this.getBinanceP2PFromAPI('sell', 'usdt')
+		this.getBinanceP2PFromAPI('buy', 'dai');
+		this.getBinanceP2PFromAPI('sell', 'dai');
+		this.getCryptosFromAPIs();
+	}
+
+	getCryptosFromAPIs() {
+		return Promise.all([
+			fetch('https://criptoya.com/api/belo/usdt/ars').then(resp => resp.json()),
+			fetch('https://criptoya.com/api/buenbit/dai/ars').then(resp => resp.json()),
+			fetch('https://criptoya.com/api/lemoncash/usdt').then(resp => resp.json()),
+			fetch('https://criptoya.com/api/ripio/usdc').then(resp => resp.json()),
+			fetch('https://criptoya.com/api/satoshitango/dai/ars').then(resp => resp.json()),
+		]).then((responseJson) => {
+			let exchanges = ['Belo', 'Buenbit', 'Lemon', 'Ripio', 'SatoshiTango'];
+			let coins = ['USDT', 'DAI', 'USDT', 'USDC', 'DAI'];
+			let api = responseJson;
+			let newApi = [];
+			for (let i = 0; i < Object.keys(api).length; i++) {
+				newApi.push({
+					id: i + 1,
+					banco: exchanges[i],
+					coin: coins[i],
+					compra: api[i].totalBid,
+					venta: api[i].totalAsk,
+					time: api[i].time
+				})
+			}
+			//console.log(newApi)
+			this.setState((state) => ({
+				cryptos: newApi
+			}));
+			//console.log(this.state);
+		})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+
 
 	getFiatFromAPI() {
 		return fetch('https://criptoya.com/api/dolar')
@@ -207,7 +293,7 @@ class App extends React.Component {
 					})
 				}
 				let filteredApi = newApi.filter(elem => elem.time * 1000 > ((new Date().getTime()) - 432000000))
-				//console.log(newApi)
+				//console.log(filteredApi)
 				this.setState((state) => ({
 					bancos: filteredApi
 				}));
@@ -218,8 +304,8 @@ class App extends React.Component {
 			});
 	}
 
-	getBinanceP2PFromAPI() {
-		return fetch('https://criptoya.com/api/binancep2p/buy/usdt/ars/20')
+	getBinanceP2PFromAPI(operation, coin) {
+		return fetch(`https://criptoya.com/api/binancep2p/${operation}/${coin}/ars/8`)
 			.then((response) => response.json())
 			.then((responseJson) => {
 				let api = responseJson.data
@@ -229,18 +315,33 @@ class App extends React.Component {
 					newApi.push({
 						id: i,
 						trader: api[keyValue[i]].advertiser.nickName,
-						traderTipo: api[keyValue[i]].advertiser.userType,
+						//traderTipo: api[keyValue[i]].advertiser.userType,
 						metodo: api[keyValue[i]].adv.tradeMethods["0"].tradeMethodName,
 						precio: api[keyValue[i]].adv.price,
+						coin: coin.toUpperCase()
 					})
 				}
 				//let filteredApi = newApi.filter(elem => elem.metodo !== "Cash in Person");
 				let filteredApi = newApi.find(elem => !elem.metodo.includes("Cash"))
-				this.setState((state) => ({
-					binanceP2P: filteredApi
-				}));
+				if (operation === 'buy' && coin === 'usdt') {
+					this.setState((state) => ({
+						binanceUSDTBuy: filteredApi
+					}));
+				} else if (operation === 'sell' && coin === 'usdt') {
+					this.setState((state) => ({
+						binanceUSDTSell: filteredApi
+					}));
+				} else if (operation === 'buy' && coin === 'dai') {
+					this.setState((state) => ({
+						binanceDAIBuy: filteredApi
+					}));
+				} else if (operation === 'sell' && coin === 'dai') {
+					this.setState((state) => ({
+						binanceDAISell: filteredApi
+					}));
+				}
 				//console.log(newApi);
-				console.log(filteredApi);
+				//console.log(filteredApi);
 			})
 			.catch((error) => {
 				console.error(error);
@@ -298,7 +399,11 @@ class App extends React.Component {
 					sortByVenta={this.sortByVenta} />
 
 				<Crypto
-					binanceP2P={this.state.binanceP2P} />
+					binanceUSDTBuy={this.state.binanceUSDTBuy}
+					binanceUSDTSell={this.state.binanceUSDTSell}
+					binanceDAIBuy={this.state.binanceDAIBuy}
+					binanceDAISell={this.state.binanceDAISell} 
+					cryptos={this.state.cryptos}/>
 
 			</div>
 		);
